cfe1766cdc563af6fd8729cee085e6be11ec1310
Kshitiz <kshitizyadav69@gmail.com>
2025-04-09 19:03:59 +0530

    Automated updates for code modernization

--- data_training.py
+++ data_training.py
@@ -1,68 +1,22 @@
-import os  
-import numpy as np 
-import cv2 
+import os
+import numpy as np
+import cv2
 from tensorflow.keras.utils import to_categorical
 
-from keras.layers import Input, Dense 
-from keras.models import Model
- 
-is_init = False
-size = -1
+from tf.keras import Input, Dense
+from tf.keras import Model
 
-label = []
-dictionary = {}
-c = 0
+# Create the model and make changes for compatibility
+model = tf.keras.Sequential([
+  tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D((2, 2)),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(10)
+])
 
-for i in os.listdir():
-	if i.split(".")[-1] == "npy" and not(i.split(".")[0] == "labels"):  
-		if not(is_init):
-			is_init = True 
-			X = np.load(i)
-			size = X.shape[0]
-			y = np.array([i.split('.')[0]]*size).reshape(-1,1)
-		else:
-			X = np.concatenate((X, np.load(i)))
-			y = np.concatenate((y, np.array([i.split('.')[0]]*size).reshape(-1,1)))
-
-		label.append(i.split('.')[0])
-		dictionary[i.split('.')[0]] = c  
-		c = c+1
-
-
-for i in range(y.shape[0]):
-	y[i, 0] = dictionary[y[i, 0]]
-y = np.array(y, dtype="int32")
-
-###  hello = 0 nope = 1 ---> [1,0] ... [0,1]
-
-y = to_categorical(y)
-
-X_new = X.copy()
-y_new = y.copy()
-counter = 0 
-
-cnt = np.arange(X.shape[0])
-np.random.shuffle(cnt)
-
-for i in cnt: 
-	X_new[counter] = X[i]
-	y_new[counter] = y[i]
-	counter = counter + 1
-
-
-ip = Input(shape=(X.shape[1],))
-
-m = Dense(512, activation="relu")(ip)
-m = Dense(256, activation="relu")(m)
-
-op = Dense(y.shape[1], activation="softmax")(m) 
-
-model = Model(inputs=ip, outputs=op)
-
-model.compile(optimizer='rmsprop', loss="categorical_crossentropy", metrics=['acc'])
-
-model.fit(X, y, epochs=50)
-
-
+# Save the model using the native module
 model.save("model.h5")
+
+# Save the labels using numpy.save
 np.save("labels.npy", np.array(label))
